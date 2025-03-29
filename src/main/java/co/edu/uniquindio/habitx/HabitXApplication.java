b73d7253fdda82061ea036acd6551171abf30ebc
Santiago Gamez Guerra <gamez.guerra18@gmail.com>
2025-03-28 20:39:51 -0500

    initial commit

---
+++ src/main/java/co/edu/uniquindio/habitx/HabitXApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.habitx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitXApplication.class, args);
+    }
+
+}
